d46d64c992fe89c13c6ebdb392933abd8e72e57c
Andrew Taft <andrew.taft@flexitanalytics.com>
2022-04-20 20:06:03 +0000

    metrics package

--- models/dim_date.sql
+++ models/dim_date.sql
@@ -6,5 +6,14 @@
 
 select
     {{ dbt_utils.surrogate_key(['date_day']) }} date_key,
-    *
+    week_start_date as date_week,
+    month_start_date as date_month,
+    quarter_start_date as date_quarter,
+    year_start_date as date_year,
+    {{
+      dbt_utils.star(
+        from=ref('stg_date_dimension'),
+        except=["week_start_date", "month_start_date", "quarter_start_date", "year_start_date"]
+      )
+    }}
 from {{ ref('stg_date_dimension') }}
