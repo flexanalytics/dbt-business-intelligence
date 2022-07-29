b4c4635cb12f4fd674039c95dc0eb1c8f5744c16
ataft <11670864+ataft@users.noreply.github.com>
2022-07-29 14:46:09 -0700

    NULL as fact table foreign key
    Example of inserting an "Unknown" row in case a dimension is missing
    https://www.kimballgroup.com/2003/02/design-tip-43-dealing-with-nulls-in-the-dimensional-model/

--- models/common/dim_customer.sql
+++ models/common/dim_customer.sql
@@ -1,3 +1,10 @@
+-- NULL as fact table foreign key: https://www.kimballgroup.com/2003/02/design-tip-43-dealing-with-nulls-in-the-dimensional-model/
+{{ config(
+    post_hook=[
+      "insert into {{ this }} values ({{ dbt_utils.surrogate_key(['-1']) }},'Unknown','Unknown','Unknown','Unknown','Unknown','Unknown','Unknown','Unknown','Unknown','Unknown')"
+    ]
+) }}
+
 select
     {{ dbt_utils.surrogate_key(['customer_code']) }} customer_key,
     *
