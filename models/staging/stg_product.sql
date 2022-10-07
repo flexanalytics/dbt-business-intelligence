1245ed86168cd1f61c163c0f5389d4c8c9319b96
ataft <11670864+ataft@users.noreply.github.com>
2022-10-07 15:18:20 -0700

    Update stg_product.sql
    fix for Redshift

--- models/staging/stg_product.sql
+++ models/staging/stg_product.sql
@@ -1,10 +1,11 @@
 -- depends_on: {{ ref('stg_sales_data') }}
 
 select distinct
-    product_code,
-    product_name,
-    product_category,
-    msrp,
-    max(order_date) OVER (PARTITION BY product_code ORDER BY order_date DESC) as recent_order_date, --slowly changing dimension Type 1, using partition
-    (select min(order_date) from {{ source('salesforce', 'stg_sales_data') }} b where a.product_code = b.product_code) as first_order_date --slowly changing dimension Type 0, using subquery
+    product_code
+    , product_name
+    , product_category
+    , msrp
+    --, max(order_date) OVER (PARTITION BY product_code ORDER BY order_date DESC) as recent_order_date --slowly changing dimension Type 1, using partition
+    , (select max(order_date) from {{ source('salesforce', 'stg_sales_data') }} b where a.product_code = b.product_code) recent_order_date --slowly changing dimension Type 1, using subquery
+    , (select min(order_date) from {{ source('salesforce', 'stg_sales_data') }} b where a.product_code = b.product_code) as first_order_date --slowly changing dimension Type 0, using subquery
 from {{ source('salesforce', 'stg_sales_data') }} a
