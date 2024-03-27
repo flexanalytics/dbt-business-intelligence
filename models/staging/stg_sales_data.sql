abba1780d47258640774d12e73f52f1313b5602b
Michael Connell <connellmp@gmail.com>
2024-03-27 16:43:48 -0700

    add incremental materialization

--- models/staging/stg_sales_data.sql
+++ models/staging/stg_sales_data.sql
@@ -1,4 +1,12 @@
-{{ config(schema='staging', materialized='table') }}
+{{
+    config(
+        schema='staging',
+        materialized='incremental',
+        unique_key='order_number',
+        incremental_strategy='merge',
+        merge_update_columns=['order_line_number', 'status']
+        )
+}}
 
 select
     {{ dbt_utils.generate_surrogate_key(['order_number','order_line_number']) }} id,
@@ -26,3 +34,7 @@ select
     target,
     {{ current_timestamp() }} data_loaded_at
 from {{ ref('sales_data_raw') }}
+
+{% if is_incremental() %}
+  where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
