5e96cf8010a0d4613e9e56e81b0257e2cc2a58ea
Andrew Taft <11670864+ataft@users.noreply.github.com>
2025-01-10 12:28:28 -0800

    Update metricflow_time_spine.sql
    Replace the date_spine logic by using the stg_date_dimension model to simplify the logic and work with SQL Server

--- models/staging/metricflow_time_spine.sql
+++ models/staging/metricflow_time_spine.sql
@@ -4,21 +4,4 @@
     )
 }}
 
-with days as (
-
-    {{
-        dbt.date_spine(
-            'day',
-            "to_date('01/01/2000','mm/dd/yyyy')",
-            "to_date('01/01/2027','mm/dd/yyyy')"
-        )
-    }}
-
-),
-
-final as (
-    select cast(date_day as date) as date_day
-    from days
-)
-
-select * from final
+select date_day from {{ ref('stg_date_dimension') }}
