8b61bd9eef1b3c566730705d0c0793d5e8f72290
Michael Connell <connellmp@gmail.com>
2025-06-03 16:43:12 -0700

    remove unnecessary order by

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -11,6 +11,6 @@ select distinct
     contact_name,
     phone,
     email,
-    max(order_date) over (partition by customer_code order by order_date desc) as recent_order_date, --slowly changing dimension type 1, using partition
-    min(order_date) over (partition by customer_code order by order_date asc) as first_order_date --slowly changing dimension type 0, using partition
+    max(order_date) over (partition by customer_code) as recent_order_date, --slowly changing dimension type 1, using partition
+    min(order_date) over (partition by customer_code) as first_order_date --slowly changing dimension type 0, using partition
 from {{ source('salesforce', 'stg_sales_data') }} as a
