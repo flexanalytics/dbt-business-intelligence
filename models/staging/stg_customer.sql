3f7cc5b52acae4076e1be7a0f253c3722e16519b
Michael Connell <connellmp@gmail.com>
2025-06-03 14:34:23 -0700

    add recent and first order dates to customer

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -10,5 +10,17 @@ select distinct
     {{ split_part('latlng', "','", 2) }} longitude,
     contact_name,
     phone,
-    email
-from {{ source('salesforce', 'stg_sales_data') }}
+    email,
+    --slowly changing dimension Type 1, using subquery
+    (
+        select max(order_date)
+        from {{ source('salesforce', 'stg_sales_data') }} as b
+        where a.customer_code = b.customer_code
+    ) as recent_order_date,
+    --slowly changing dimension Type 0, using subquery
+    (
+        select min(order_date)
+        from {{ source('salesforce', 'stg_sales_data') }} as b
+        where a.customer_code = b.customer_code
+    ) as first_order_date
+from {{ source('salesforce', 'stg_sales_data') }} as a
