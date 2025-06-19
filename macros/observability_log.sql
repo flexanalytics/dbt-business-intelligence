f3df9a9cdeecba9752d6ce29d9f0a70e47603a2d
Andrew Taft <11670864+ataft@users.noreply.github.com>
2025-06-19 14:58:45 -0700

    Update observability_log.sql
    Use A href tag

--- macros/observability_log.sql
+++ macros/observability_log.sql
@@ -1,4 +1,4 @@
 {% macro default__log_observability() -%}
-  {% do log("dbt Invocation ID: " ~ invocation_id, true) %}
-  {% do log("Observability Url: " ~ 'http://flex_nuc:3030/?specs={"filters":[{"label": "Command ID", "id":"dim_invocation.command_invocation_id","selections":[{"use":"' ~ invocation_id ~ '"}]}]}#analysis/3b5c2b32-29ab-4805-b8de-f25c9261e1cc', true) %}
+    {% do log("dbt Invocation ID: " ~ invocation_id, true) %}
+    {% do log('<a href="http://flex_nuc:3030/?specs={%22filters%22:[{%22label%22:%22Command ID%22,%22id%22:%22dim_invocation.command_invocation_id%22,%22selections%22:[{%22use%22:%22' ~ invocation_id ~ '%22}]}]}#analysis/observability-report">Observability Report</a>', true) %}
 {%- endmacro %}
