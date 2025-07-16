719770c7a56bd547835e4afc6a9795325d970780
Andrew Taft <11670864+ataft@users.noreply.github.com>
2025-07-16 15:11:21 -0700

    Add Customer Region (#60)
    Add customer region to dim_customer and staging table

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -8,9 +8,10 @@ select distinct
     country,
     {{ split_part('latlng', "','", 1) }} latitude,
     {{ split_part('latlng', "','", 2) }} longitude,
+    region,
     contact_name,
     phone,
     email,
     max(order_date) over (partition by customer_code) as recent_order_date, --slowly changing dimension type 1, using partition
     min(order_date) over (partition by customer_code) as first_order_date --slowly changing dimension type 0, using partition
-from {{ source('salesforce', 'stg_sales_data') }} as a
+from {{ source('salesforce', 'stg_sales_data') }}
